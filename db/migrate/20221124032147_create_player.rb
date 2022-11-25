03b453f02c32f43b917d2aba9bac2b91da899163
PepeLira <jflira@miuandes.cl>
2022-11-25 19:14:40 -0300

    first commit

---
+++ db/migrate/20221124032147_create_player.rb
@@ -0,0 +1,16 @@
+class CreatePlayer < ActiveRecord::Migration[7.0]
+  def change
+    create_table :players do |t|
+      t.integer :chat_id, unique: true
+      t.integer :telegram_id, unique: true
+      t.string :chat_name
+      t.string :username
+      t.string :first_name
+      t.integer :numbers_wins
+      t.integer :trivia_wins
+      t.integer :hangman_wins
+
+      t.timestamps
+    end
+  end
+end
